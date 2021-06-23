c6db16fbe29be814c4be7050bf046c6d91e113e5
VuThiBichThu <vuthibichthu.it@gmail.com>
2021-06-23 15:54:29 +0700

    create UI show all workspaces

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import routes from "./routes";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import TheLayout from "./containers/layouts/TheLayout/TheLayout";
 
 function App() {
   return (
-    <div> HR Resourcing App </div>
+    <Router>
+      <Switch>
+        {routes.map((route, idx) => {
+          return (
+            route.component && (
+              <Route
+                key={idx}
+                path={route.path}
+                exact={route.exact}
+                component={TheLayout(route.component)}
+              />
+            )
+          );
+        })}
+        <Redirect from="/" to="/workspaces" />
+      </Switch>
+    </Router>
   );
 }
 

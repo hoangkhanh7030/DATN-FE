9679a84db3eb7e7eedf30be21966610528db9b58
VuThiBichThu <vuthibichthu.it@gmail.com>
2021-07-28 11:51:53 +0700

    fix code reviews - resources table

--- src/containers/resources/table/Table.js
+++ src/containers/resources/table/Table.js
@@ -1,22 +1,12 @@
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableContainer,
-  TableRow,
-  Typography,
-} from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
+import { Paper, Table, TableBody, TableContainer } from "@material-ui/core";
 import { INITIAL_ROWS_PER_PAGE } from "constants/index";
 import React from "react";
-import { StyledTableCell, useStyles } from "./style";
+import { useStyles } from "./style";
 import TableHeader from "./TableHeader";
 import EnhancedTableRow from "./TableRow";
 import * as _ from "underscore";
-
-const COLS = [1, 2, 3, 4, 5, 6];
-const ROWS = [1, 2, 3, 4, 5];
-const EMPTY_TABLE = "No resources to load.";
+import { LoadingTable } from "components/resources/LoadingTable";
+import { EmptyRows } from "components/resources/EmptyRows";
 
 export default function ResourcesTable(props) {
   const {
@@ -28,53 +18,29 @@ export default function ResourcesTable(props) {
   } = props;
   const classes = useStyles({ emptyRows });
 
-  const noData = () => {
-    return data.length <= 0;
-  };
-  const noEmptyRows = () => {
-    return emptyRows <= 0 || noData();
-  };
-
   return (
     <TableContainer component={Paper} className={classes.root} elevation={0}>
       <Table className={classes.table}>
         <TableHeader classes={classes} handleSort={handleSort} />
         <TableBody>
           {isLoading ? (
-            ROWS.map((el) => (
-              <TableRow key={el}>
-                {COLS.map((el) => (
-                  <StyledTableCell key={el} height="26">
-                    <Skeleton />
-                  </StyledTableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : noData() ? (
-            <TableRow className={classes.emptyRows}>
-              <StyledTableCell colSpan={6}>
-                <Typography color="primary" align="center">
-                  {EMPTY_TABLE}
-                </Typography>
-              </StyledTableCell>
-            </TableRow>
+            <LoadingTable />
           ) : (
-            data.map((row) => {
-              return (
+            <>
+              {data.map((row) => (
                 <EnhancedTableRow
                   key={_.get(row, "id")}
                   row={row}
                   handleOpenDialog={handleOpenDialog}
-                ></EnhancedTableRow>
-              );
-            })
-          )}
-          {noEmptyRows() ? (
-            <></>
-          ) : (
-            <TableRow className={classes.emptyRows}>
-              <StyledTableCell colSpan={6} />
-            </TableRow>
+                />
+              ))}
+
+              <EmptyRows
+                isFullPage={!emptyRows}
+                isEmptyTable={data.length === 0}
+                rowHeight={classes.emptyRows}
+              />
+            </>
           )}
         </TableBody>
       </Table>
